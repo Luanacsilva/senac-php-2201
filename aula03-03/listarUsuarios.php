dba48dc899370d8497d3f6a29b537661962fc078
Luana <lucris_231@hotmail.com>
2022-03-17 22:11:07 -0300

    pdo primeira parte

--- aula03-03/listarUsuarios.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once'funcoes/funcoesAluno.php';
-
-listarAluno();
--- aula10-03/recebe.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$usuario = $_POST['usuario'];
-
-echo "$usuario <br>";
-
-for($i = 1; $i <= $_POST['numero']; $i++)
-    echo "{$_POST['numero']} <br>";
-    
